e9f0ab66fd25451108577ce899728002c20f05af
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-27 12:01:28 +0530

    [VINAY] Add:Displaying Employee Attendence

---
+++ Demo2Example/EmployeeAttendece.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2Example
+{
+   public class EmployeeAttendece
+    {
+        const int IS_PRESENT =1;
+        const int IS_NOT_PRESENT =0;
+        public  void DisplayAttendence()
+        {
+            Random random = new Random();
+            int check=random.Next(0, 2);
+            switch (check)
+            { 
+            case IS_PRESENT:
+                    Console.WriteLine("Employee is Present");
+                    break;
+
+                case IS_NOT_PRESENT:
+                    Console.WriteLine("Employee is not Present");
+                   
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ Demo2Example/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Demo2Example;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        EmployeeAttendece employee = new EmployeeAttendece();
+        employee.DisplayAttendence();
+        Console.WriteLine("Enter the Employee Performance");
+        string performane=Console.ReadLine();
+        Console.WriteLine("result"+performane);
+        Console.WriteLine("Enter the Employee Rank");
+        int rank =Convert.ToInt32( Console.ReadLine());
+        Console.WriteLine("Rank is:" + rank);
+    }
+}
